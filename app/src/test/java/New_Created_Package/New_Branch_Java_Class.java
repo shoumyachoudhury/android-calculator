22a9f52f9af09c18701b4a99168ad647229f3168
Shoumya <shoummochoudhury@yahoo.com>
2022-05-08 16:19:37 +0600

    Commiting to create conflict

--- app/src/test/java/New_Created_Package/New_Branch_Java_Class.java
+++ app/src/test/java/New_Created_Package/New_Branch_Java_Class.java
@@ -3,6 +3,15 @@ package New_Created_Package;
 public class New_Branch_Java_Class {
     int x = 50, y = 60;
     String message = "RRR";
+    String conflictMessage = "Branch to create conflict";
+
+    public String getConflictMessage() {
+        return conflictMessage;
+    }
+
+    public void setConflictMessage(String conflictMessage) {
+        this.conflictMessage = conflictMessage;
+    }
 
     public int getX() {
         return x;
