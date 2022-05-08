c5fd6227da9a27518e3ceb1abcacff2fc6174efb
Shoumya <shoummochoudhury@yahoo.com>
2022-05-08 16:06:52 +0600

    First commit from Master Branch.

---
+++ app/src/test/java/New_Created_Package/Created_JAVA_Class.java
@@ -0,0 +1,36 @@
+package New_Created_Package;
+
+public class Created_JAVA_Class {
+    int var1 = 10, var2 = 20;
+    String str= "Hello World";
+
+    public int getVar1() {
+        return var1;
+    }
+
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
+
+    public int getVar2() {
+        return var2;
+    }
+
+    public void setVar2(int var2) {
+        this.var2 = var2;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public Created_JAVA_Class(int var1, int var2, String str) {
+        this.var1 = var1;
+        this.var2 = var2;
+        this.str = str;
+    }
+}
